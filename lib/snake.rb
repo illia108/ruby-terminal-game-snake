99389ab5fb561d590895211f07b225c12a2e17e5
BogdanLinnik <bogdya.linnik@gmail.com>
2017-10-17 14:35:48 +0300

    in lib/snake.rb change if ! on unless

--- lib/snake.rb
+++ lib/snake.rb
@@ -6,7 +6,7 @@ class Snake
     @cells = field.cells
     @size = field.size
   end
-  
+
   def create_snake
     @snake = []
     @row = @size - 2
@@ -16,24 +16,24 @@ class Snake
       @snake[i] = {'row' => @row, 'cell' => @cell + i}
     end
   end
-  
+
   def move(direction)
     @direction = direction
-    
+
     # find out where is a head and a tail of a snake
     @row_tail = @snake[0]['row']
     @cell_tail = @snake[0]['cell']
     @row_head = @snake[@snake.length - 1]['row']
     @cell_head = @snake[@snake.length - 1]['cell']
-    
+
     # prevent moving backward
-    if !@current_direction.nil?
+    unless @current_direction.nil?
       if @direction['row'] == @current_direction['row'] * -1 || @direction['cell'] == @current_direction['cell'] * -1
         @direction = @current_direction
       end
     end
-    
-    
+
+
     if @cells[@row_head + @direction['row']][@cell_head + @direction['cell']] == Field::BLACKSQ
       Func.game_over
     else
@@ -41,15 +41,15 @@ class Snake
       @snake.push('row' => @row_head + @direction['row'], 'cell' => @cell_head + @direction['cell'])
       @current_direction = @direction
     end
-    
+
     # remove tail or eat rabbit
     if @row_head == @rabbit.location['row'] && @cell_head == @rabbit.location['cell']
-      @field.score += 1 
+      @field.score += 1
       @rabbit.create_rabbit
     else
       @cells[@row_tail][@cell_tail] = Field::WHITESQ
       @snake.shift
     end
   end
-  
+
 end
